0898561132b9be437be4a06010affea57a42d6a6
root <root@server.gvarc.ir>
2017-08-06 00:42:28 -0400

    implement of show quran in robot

--- routes/telegram.php
+++ routes/telegram.php
@@ -5,7 +5,7 @@ use XB\theory\Shoot;
 // // use XB\telegramObjects\InlineKeyboardMarkup;
 // use XB\telegramObjects\InlineKeyboardButton;
 
-Shoot::trigger(function($u){
+$this->trigger(function($u){
     return true;
 },function($u){
     Storage::put("updates/last.json", "$u\n\n".
@@ -13,12 +13,35 @@ Shoot::trigger(function($u){
     );
 });
 
-// Shoot::trigger(function($u){return true;},'sayHello@test');
+/*
+$this->trigger(function($u){return true;},function($u){
+    empty($u->message->from->first_name)  or print "\nmessage:".$u->message->from->first_name;
+    empty($u->callback_query->from->first_name)  or print "\ncallback_query:".$u->callback_query->from->first_name;
+    // print_r($u);die();
+    });//*/
 
-Shoot::trigger(function(&$u){
+$this->trigger(function(&$u){
     return !empty($u->message->text) && $u->message->text=='/start';
 },'start@showMenu');
 
-Shoot::trigger(function(&$u){
+$this->trigger(function(&$u){
     return !empty($u->message->text) && $u->message->text=='درباره ما';
 },'start@aboutUs');
+
+$this->trigger(function(&$u){
+    return !empty($u->message->text) && $u->message->text=='نمایش قرآن';
+},'quran@show');
+
+$this->trigger(function($u,&$s){
+    if(empty($u->callback_query->data)){
+        return false;
+    }
+    $data=explode('/',$u->callback_query->data);
+    if($data[0]=='page' && 
+        is_numeric($data[1]) &&
+        $data[1]>=1 && $data[1]<=604){
+            $s=(int)$data[1];
+            return true;
+        }
+    return false;
+},'quran@goto');
